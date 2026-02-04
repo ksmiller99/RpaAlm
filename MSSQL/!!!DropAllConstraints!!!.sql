04b113c84c8c5c3e392fd96b4a06f088ae2d69f2
Kevin S. Miller <kmille39@its.jnj.com>
2026-02-04 07:13:02 -0500

    Improved the print statement to handle large strings

--- MSSQL/!!!DropAllConstraints!!!.sql
+++ MSSQL/!!!DropAllConstraints!!!.sql
@@ -24,7 +24,15 @@ WHERE c.[type] IN ('D', 'C', 'F', 'PK', 'UQ') -- Types: Default, Check, Foreign 
 ORDER BY c.[type]; -- Ordering helps avoid issues where a FK relies on a PK/UQ
 
 -- Print the generated script to review it (optional, limited to 4000 chars in print output)
-PRINT @sql;
+-- Optional: Print the generated script to review before execution
+DECLARE @size INT = LEN(@sql);
+DECLARE @count INT = 0;
+WHILE @size > 0
+BEGIN
+    PRINT SUBSTRING(@sql,(@count*4000)+1,IIF(@size>4000,4000,@size));
+	SET @size = @size-4000;
+	SET @count = @count+1;
+END
 
 -- Uncomment the following line to execute the script
 -- EXEC sys.sp_executesql @sql;
